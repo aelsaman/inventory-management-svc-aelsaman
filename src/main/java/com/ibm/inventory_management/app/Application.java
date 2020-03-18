4dfbeb076a82071ecc2d9b5098c9bbdeba70182a
Alaa ElSaman <alaa.elsaman@gmail.com>
2020-03-18 21:44:26 +0400

    Adds StockItemController

--- src/main/java/com/ibm/inventory_management/app/Application.java
+++ src/main/java/com/ibm/inventory_management/app/Application.java
@@ -15,24 +15,23 @@ import org.springframework.core.env.Environment;
 @ComponentScan({"com.ibm.inventory_management.*", "com.ibm.cloud_garage.*", "com.ibm.health"})
 public class Application extends SpringBootServletInitializer {
 
-    @Autowired
-    Environment environment;
+  @Autowired Environment environment;
 
-    public static void main(String[] args) {
-        SpringApplication.run(com.ibm.inventory_management.app.Application.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(com.ibm.inventory_management.app.Application.class, args);
+  }
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-            String port = environment.getProperty("local.server.port");
-            System.out.println();
-            System.out.println("Server started - http://localhost:" + port + "/swagger-ui.html");
-        };
-    }
+  @Bean
+  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+    return args -> {
+      String port = environment.getProperty("local.server.port");
+      System.out.println();
+      System.out.println("Server started - http://localhost:" + port + "/swagger-ui.html");
+    };
+  }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Application.class);
+  }
 }
